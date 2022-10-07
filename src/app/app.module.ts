79d0c33533a72b0424d2a8c5be034ea93a52dbd8
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-07 15:46:03 +0530

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchstudentComponent } from './searchstudent/searchstudent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchstudentComponent
   ],
   imports: [
     BrowserModule,
